0dde26fc0253e4851a002d72422ab95b593afbfa
Thomas Maynard <90271276+Thomas4897@users.noreply.github.com>
2022-05-21 02:34:47 +0800

    Renaming .js

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
+//* spread operator, rest operator and Deconstructing data
+
 const userPersonalData = {
-  name: 'peter',
-  age: '56',
-  birthday: 'jan 1st',
- };
- const userGameData = {
+  name: "peter",
+  age: "56",
+  birthday: "jan 1st",
+};
+const userGameData = {
   score: 4546,
-  accomplishments: ['won award for being good gamer', 'won 1st win', 'got good score on the weekend'],
- };
-  
+  accomplishments: [
+    "won award for being good gamer",
+    "won 1st win",
+    "got good score on the weekend",
+  ],
+};
 
 // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // 1. Combine the personalData and userGameData into a user object that is equal to the object below, by using the spread operator:
@@ -20,53 +25,42 @@ const userPersonalData = {
 // }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-
-
-
 // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // 2. Make a copy of your new user object but overrode the birthday to december 31st
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
- 
 
 // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // 3. Use the spread operator to make a copy of the accomplishments array and store it i na new variable
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-
-
 // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// 
+//
 //  4.Given the object bellow, use object destructuring to get the favorite food value (user.name.favoriteThings.food)
 //  and store it in a variable name food
-// 
+//
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 var user = {
-  name: 'pete',
-  age: '32',
+  name: "pete",
+  age: "32",
   favoriteThings: {
-    food: ['pizza', 'tacos', 'burgers', 'italian'],
+    food: ["pizza", "tacos", "burgers", "italian"],
     movies: [],
   },
- };
- 
+};
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // 5. Once you have grabbed the favorite foods. Descontrusct the food array to grab only the first 2 values. //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// 6. use object destructuring and the rest operator to transform the following array into 3 variables: name, age, and food. 
+// 6. use object destructuring and the rest operator to transform the following array into 3 variables: name, age, and food.
 //    the food variable should have all the array items starting from the third one.
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-const data = ['peter', '34', 'apple', 'oranges', 'pizza', 'tacos'];
-
-
+const data = ["peter", "34", "apple", "oranges", "pizza", "tacos"];
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// 7. use object destructuring to grab the following from the userData object: 
+// 7. use object destructuring to grab the following from the userData object:
 // - The user's name and in a constant named userName.
 // - The user's facorite food array and name it favoriteFood.
 // - The users first favorite thing (cars) and name it favoriteThing
@@ -74,14 +68,13 @@ const data = ['peter', '34', 'apple', 'oranges', 'pizza', 'tacos'];
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const userData = {
-  name: 'Peter',
+  name: "Peter",
   favorites: {
     needs: {
-      food:  ['burger', 'pizza', 'tacos', 'fried chicken', 'sushi'],
+      food: ["burger", "pizza", "tacos", "fried chicken", "sushi"],
     },
     wants: {
-      things: ['cars', 'jewelry'],
+      things: ["cars", "jewelry"],
     },
   },
 };
-
