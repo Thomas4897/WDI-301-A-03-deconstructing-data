4de9a2473ea03274a525dbe3d7005bb6da09e7a3
Thomas Maynard <90271276+Thomas4897@users.noreply.github.com>
2022-05-21 03:40:43 +0800

    Adding Code Answers

--- main.js
+++ main.js
@@ -25,13 +25,32 @@ const userGameData = {
 // }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// 2. Make a copy of your new user object but overrode the birthday to december 31st
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//! Function that combines two seperate Objects into one Object
+const combineObjects = (arr1, arr2) => {
+  return { ...arr1, ...arr2 };
+};
+
+const newUserInfo = combineObjects(userPersonalData, userGameData);
+
+console.log("\ncombineObjects:", newUserInfo);
 
 // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// 3. Use the spread operator to make a copy of the accomplishments array and store it i na new variable
+// 2. Make a copy of your new user object but override the birthday to december 31st
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//! Creates a copy of an Object and Updates 'birthday' value
+const updatedCopiedObject = { ...newUserInfo, birthday: "december 31st" };
+
+console.log("\ncopiedObject:", updatedCopiedObject);
+
+// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 3. Use the spread operator to make a copy of the accomplishments array and store it in a new variable
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//! Copy the 'accomplishments' values into a new array
+const copyOfAccomplishments = [...updatedCopiedObject.accomplishments];
+
+console.log("\ncopyOfAccomplishments:", copyOfAccomplishments);
 
 // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -49,20 +68,37 @@ var user = {
   },
 };
 
+//! Nested Destructing
+const {
+  favoriteThings: { food },
+} = user;
+
+console.log("\nfood:", food);
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // 5. Once you have grabbed the favorite foods. Descontrusct the food array to grab only the first 2 values. //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+//! Deconstruct only first two values
+const [firstValue, secondValue] = food;
+
+console.log("\nFirst two values of food Array:", firstValue, secondValue);
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // 6. use object destructuring and the rest operator to transform the following array into 3 variables: name, age, and food.
-//    the food variable should have all the array items starting from the third one.
+// The food variable should have all the array items starting from the third one.
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const data = ["peter", "34", "apple", "oranges", "pizza", "tacos"];
+
+//! Deconstrunct and Rest
+const [name, age, ...food2] = data;
+
+console.log(`\nDeconstruct and Rest: ${name}, ${age}, ${food2}`);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // 7. use object destructuring to grab the following from the userData object:
 // - The user's name and in a constant named userName.
-// - The user's facorite food array and name it favoriteFood.
+// - The user's favorite food array and name it favoriteFood.
 // - The users first favorite thing (cars) and name it favoriteThing
 // - The users second favorite thing (jewelry) and name it secondfavoriteThing
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -78,3 +114,20 @@ const userData = {
     },
   },
 };
+
+//! Nested Duconstructing and Renaming
+const {
+  name: userName,
+  favorites: {
+    needs: { food: favoriteFood },
+    wants: {
+      things: [favoriteThing, secondfavoriteThing],
+    },
+  },
+} = userData;
+
+console.log(
+  `\nuserName: ${userName}, favoriteFood: ${favoriteFood}, favoriteThing: ${favoriteThing}, secondfavoriteThing: ${secondfavoriteThing}`
+);
+
+console.log("");
